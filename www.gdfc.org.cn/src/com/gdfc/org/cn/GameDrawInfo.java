4e5a8d0020a915713c8e3f2e961cb8f519c1a6b4
crane.ding <crane.ding@0cf19388-cbb7-11de-b058-13285e7dd63e>
2009-11-12 05:49:32 +0000

    优化代码

--- www.gdfc.org.cn/src/com/gdfc/org/cn/GameDrawInfo.java
+++ www.gdfc.org.cn/src/com/gdfc/org/cn/GameDrawInfo.java
@@ -3,22 +3,95 @@
  */
 package com.gdfc.org.cn;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author crane.ding
  * 
  */
 public class GameDrawInfo {
+	/*
+		编号:5 期号:2009132 开奖号码:04141521233007, -双色球
+		编号:6 期号:2009308 开奖号码: 0 3 0,  -3D
+		编号:1 期号:2009308 开奖号码:01040715242617, -36选7
+		编号:8 期号:2009308 开奖号码:17 龙 夏 东 -好彩1
+	
+		编号:2 期号:2009132 开奖号码:0210141523, -26选5
+	
+		编号:7 期号:2009066 开奖号码:0304081012163231, -深圳风采
+	*/
+	static enum GameName {
+		_双色球("5", "双色球"),
+		_3D("6", "3D"),
+		_36选7("1", "36选7"),
+		_好彩1("8", "好彩1"),
+		_26选5("2", "26选5"),
+		_深圳风采("7", "深圳风采"),
+		;
+		private String gameID;
+		private String gameName;
 
+		private GameName(String gameID, String gameName) {
+			this.gameID = gameID;
+			this.gameName = gameName;
+		}
+
+		public String getGameID() {
+			return gameID;
+		}
+
+		public String getGameName() {
+			return gameName;
+		}
+		
+		public static GameName getGameName(String gameID){
+			if(_双色球.gameID.equals(gameID))
+				return _双色球;
+			if(_3D.gameID.equals(gameID))
+				return _3D;
+			if(_36选7.gameID.equals(gameID))
+				return _36选7;
+			if(_好彩1.gameID.equals(gameID))
+				return _好彩1;
+			if(_26选5.gameID.equals(gameID))
+				return _26选5;
+			if(_好彩1.gameID.equals(gameID))
+				return _好彩1;
+			if(_深圳风采.gameID.equals(gameID))
+				return _深圳风采;
+			return null;
+		}
+	}
+	
 	/*
 	 * "rollNext":11028021, "drawID":1578, "playTime":"周一至周日 21:54",
 	 * "luckyNo":"01040715242617", "gameID":1, "drawName":"2009308"
 	 */
 
+	/**
+	 * 下次开奖时间
+	 */
 	private String rollNext;
+	/**
+	 * 期号
+	 */
 	private String drawID;
+	/**
+	 * 开奖日
+	 */
 	private String playTime;
+	/**
+	 * 开奖号码
+	 */
 	private String luckyNo;
+	/**
+	 * 编号
+	 */
 	private String gameID;
+	/**
+	 * 期名
+	 */
 	private String drawName;
 
 	public String getRollNext() {
@@ -69,4 +142,55 @@ public class GameDrawInfo {
 		this.drawName = drawName;
 	}
 
+	@Override
+	public String toString() {
+		final String nextLine = System.getProperty("line.separator");
+		final StringBuffer buff = new StringBuffer()
+			.append(nextLine).append("-------------------------")
+			.append(nextLine).append(" 彩票编号:").append(gameID)
+			.append(nextLine).append(" 彩票玩法:").append(GameName.getGameName(gameID).getGameName())
+			.append(nextLine).append(" 彩票期号:").append(drawID)
+			.append(nextLine).append(" 彩票期名:").append(drawName)
+			.append(nextLine).append(" 开奖号码:").append(LuckeyNoArrays())
+			.append(nextLine).append(" 开奖周期:").append(playTime)
+			.append(nextLine).append(" 下期时间:").append(rollNext)
+			.append(nextLine).append("-------------------------")
+			;
+		
+		return buff.toString();
+	}
+	
+	/**
+	 * 开奖号码信息
+	 */
+	public List<String> LuckeyNoArrays() {
+		int endIndex = 0;
+		final List<String> arrays = new ArrayList<String>();
+		final GameName gameName = GameName.getGameName(gameID);
+
+		switch (gameName) {
+		case _3D:
+			endIndex = 6;
+			break;
+		case _好彩1:
+			endIndex = 8;
+			break;
+		case _26选5:
+			endIndex = 10;
+			break;
+		case _双色球:
+		case _36选7:
+			endIndex = 14;
+			break;
+		case _深圳风采:
+			endIndex = 16;
+			break;
+		}
+
+		for (int i = 0; i < endIndex; i = i + 2) {
+			arrays.add(luckyNo.substring(i, i + 2));
+		}
+
+		return arrays;
+	}
 }
--- www.gdfc.org.cn/src/com/gdfc/org/cn/GameDrawInfosHelper.java
+++ www.gdfc.org.cn/src/com/gdfc/org/cn/GameDrawInfosHelper.java
@@ -5,19 +5,25 @@ package com.gdfc.org.cn;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
+ * 福彩开奖号码工具类
  * @author crane.ding
  * at drawinfos.js |iconv -c -f utf8 -t gbk |grep -E -o 'luckyNo":"[^"]+' |awk -F'"' '{print $NF}' |sed "s/\([0-9]\{2\}\)/\1 /g" 
  */
 public class GameDrawInfosHelper {
 
+	private static String spec = "http://www.gdfc.org.cn/datas/drawinfos.js";
+	private static int connectTimeout = 10000;
+	private static int readTimeOut = 10000;
+	
 	static List<GameDrawInfo> drawInfos = new ArrayList<GameDrawInfo>();
 	
 	/**
@@ -25,49 +31,87 @@ public class GameDrawInfosHelper {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-		URL url = new URL("http://www.gdfc.org.cn/datas/drawinfos.js");
+		// 返回信息
+		final String response = doGet();
 		
-		HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+		// 解析成GameDrawInfo对象
+		List<GameDrawInfo> gameDrawInfos = toDrawInfos(response);
 		
-		connection.setConnectTimeout(10000);
-		connection.setReadTimeout(10000);
+		// 打印
+		System.out.println(gameDrawInfos);
+	}
+	
+	private static String doGet() throws IOException{
+		final URL url = new URL(spec);
+		final HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+		
+		connection.setConnectTimeout(connectTimeout);
+		connection.setReadTimeout(readTimeOut);
 		connection.connect();
 
 		try {
 			if(connection.getResponseCode() == 200){
 				// 成功
-				String string = InputStream2String(connection.getInputStream());
-				System.out.println(string);
+				String str;
+				final BufferedReader breader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "GBK"));
+				final StringBuffer sb = new StringBuffer();
+
+				while ((str = breader.readLine()) != null) {
+					sb.append(str);
+				}
 				
-				torawInfos(string);
+				return sb.toString();
 			}else {
 				// 失败
 				System.err.println(connection.getResponseCode());
+				return null;
 			}
 		} finally {
 			connection.disconnect();
 		}
+		
 	}
 	
-	private static void torawInfos(String string) {
-		//var gameDrawInfos = [{"rollNext":11028021,"drawID":1578,"playTime":"周一至周日 21:54","luckyNo":"01040715242617","gameID":1,"drawName":"2009308"},{"rollNext":0,"drawID":1155,"playTime":"周二、周四、周日 21:54","luckyNo":"0210141523","gameID":2,"drawName":"2009132"},{"rollNext":68350516,"drawID":2009132,"playTime":"周二、周四、周日 20:45","luckyNo":"04141521233007","gameID":5,"drawName":"2009132"},{"rollNext":0,"drawID":2009308,"playTime":"周一至周日 20:30","luckyNo":" 0 3 0","gameID":6,"drawName":"2009308"},{"rollNext":4041787,"drawID":2009066,"playTime":"周二、周五 20:08","luckyNo":"0304081012163231","gameID":7,"drawName":"2009066"},{"rollNext":0,"drawID":1578,"playTime":"周一至周日 21:54","luckyNo":"17 龙 夏 东","gameID":8,"drawName":"2009308"}];
-		
-		System.out.println(string.matches(".*(\\{.*\\}),(\\{.*\\}),(\\{.*\\}),(\\{.*\\}),(\\{.*\\}),(\\{.*\\}).*"));
-		
-		//(.*)({.*}),({.*}),({.*}),({.*}),({.*}),({.*}).*
-		
-	}
+	private static List<GameDrawInfo> toDrawInfos(String message) {
+		// var gameDrawInfos = [{"rollNext":11028021,"drawID":1578,"playTime":"周一至周日 21:54","luckyNo":"01040715242617","gameID":1,"drawName":"2009308"},{"rollNext":0,"drawID":1155,"playTime":"周二、周四、周日 21:54","luckyNo":"0210141523","gameID":2,"drawName":"2009132"},{"rollNext":68350516,"drawID":2009132,"playTime":"周二、周四、周日 20:45","luckyNo":"04141521233007","gameID":5,"drawName":"2009132"},{"rollNext":0,"drawID":2009308,"playTime":"周一至周日 20:30","luckyNo":" 0 3 0","gameID":6,"drawName":"2009308"},{"rollNext":4041787,"drawID":2009066,"playTime":"周二、周五 20:08","luckyNo":"0304081012163231","gameID":7,"drawName":"2009066"},{"rollNext":0,"drawID":1578,"playTime":"周一至周日 21:54","luckyNo":"17 龙 夏 东","gameID":8,"drawName":"2009308"}];
+		final List<GameDrawInfo> gameDrawInfos = new ArrayList<GameDrawInfo>();
+		final Pattern pattern = Pattern.compile(".*(\\{.*\\}),(\\{.*\\}),(\\{.*\\}),(\\{.*\\}),(\\{.*\\}),(\\{.*\\}).*");
+		final Matcher matcher = pattern.matcher(message);
 
-	private static synchronized String InputStream2String(InputStream is) throws IOException {
-		String str;
-		final BufferedReader breader = new BufferedReader(new InputStreamReader(is, "GBK"));
-		final StringBuffer sb = new StringBuffer();
-
-		while ((str = breader.readLine()) != null) {
-			sb.append(str);
+		// 匹配不成功
+		if(!matcher.matches())
+			return gameDrawInfos;
+		
+		final int groupCount = matcher.groupCount();
+		for (int i = 1; i <= groupCount; i++) {
+			String record = matcher.group(i);
+			// 清除中括号([])和双引号("")
+			record = record.substring(1, record.length() - 1).replace("\"", "");
+			// rollNext:0,drawID:1578,playTime:周一至周日 21:54,luckyNo:17 龙 夏 东,gameID:8,drawName:2009308
+			String[] fields = record.split("\\,");
+			
+			final GameDrawInfo drawInfo = new GameDrawInfo();
+			for (String fs : fields) {
+				final String[] field = fs.split("\\:");
+				if(field[0].equals("rollNext")){
+					drawInfo.setRollNext(field[1]);
+				} else if(field[0].equals("drawID")){
+					drawInfo.setDrawID(field[1]);
+				} else if(field[0].equals("playTime")){
+					drawInfo.setPlayTime(field[1]);
+				} else if(field[0].equals("luckyNo")){
+					drawInfo.setLuckyNo(field[1]);
+				} else if(field[0].equals("gameID")){
+					drawInfo.setGameID(field[1]);
+				} else if(field[0].equals("drawName")){
+					drawInfo.setDrawName(field[1]);
+				} 
+			}
+			
+			gameDrawInfos.add(drawInfo);
 		}
 		
-		return sb.toString();
+		return gameDrawInfos;
 	}
 
 }
