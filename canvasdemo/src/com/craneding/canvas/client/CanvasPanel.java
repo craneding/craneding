3b00040d9449319dd23dee9caed84926ef44b7de
crane.ding <crane.ding@0cf19388-cbb7-11de-b058-13285e7dd63e>
2010-04-07 15:17:32 +0000

    新增鼠标事件

--- canvasdemo/src/com/craneding/canvas/client/CanvasPanel.java
+++ canvasdemo/src/com/craneding/canvas/client/CanvasPanel.java
@@ -4,7 +4,18 @@
 package com.craneding.canvas.client;
 
 import com.google.gwt.dom.client.Document;
+import com.google.gwt.event.dom.client.MouseDownEvent;
+import com.google.gwt.event.dom.client.MouseDownHandler;
+import com.google.gwt.event.dom.client.MouseMoveEvent;
+import com.google.gwt.event.dom.client.MouseMoveHandler;
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.MouseOverEvent;
+import com.google.gwt.event.dom.client.MouseOverHandler;
+import com.google.gwt.event.dom.client.MouseUpEvent;
+import com.google.gwt.event.dom.client.MouseUpHandler;
 import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
@@ -12,11 +23,33 @@ import com.google.gwt.user.client.ui.Widget;
  * 
  */
 public class CanvasPanel extends Widget {
-
+	
 	public CanvasPanel() {
 		setElement(Document.get().createElement("canvas"));
+		
+		sinkEvents(Event.MOUSEEVENTS);
 	}
-
+	
+	public void addMouseDownHandler(MouseDownHandler downHandler){
+		addDomHandler(downHandler, MouseDownEvent.getType());
+	}
+	
+	public void addMouseUpHandler(MouseUpHandler upHandler){
+		addDomHandler(upHandler, MouseUpEvent.getType());
+	}
+	
+	public void addMouseOverHandler(MouseOverHandler overHandler){
+		addDomHandler(overHandler, MouseOverEvent.getType());
+	}
+	
+	public void addMouseOverHandler(MouseOutHandler outHandler){
+		addDomHandler(outHandler, MouseOutEvent.getType());
+	}
+	
+	public void addMouseMoveHandler(MouseMoveHandler moveHandler){
+		addDomHandler(moveHandler, MouseMoveEvent.getType());
+	}
+	
 	public Canvas getCanvas() {
 		return (Canvas) getElement();
 	}
