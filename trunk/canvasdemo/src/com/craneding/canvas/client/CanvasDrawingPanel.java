52ac793850087b5cb0f38c19f2286af92f5712c8
crane.ding <crane.ding@0cf19388-cbb7-11de-b058-13285e7dd63e>
2010-04-11 13:07:44 +0000

    优化代码

--- trunk/canvasdemo/src/com/craneding/canvas/client/CanvasDrawingPanel.java
+++ trunk/canvasdemo/src/com/craneding/canvas/client/CanvasDrawingPanel.java
@@ -3,8 +3,14 @@
  */
 package com.craneding.canvas.client;
 
-import com.craneding.canvas.client.drawtools.BrushCanvas;
-import com.craneding.canvas.client.drawtools.RectangleCanvas;
+import com.craneding.canvas.client.drawtools.Ellipse;
+import com.craneding.canvas.client.drawtools.Pencil;
+import com.craneding.canvas.client.drawtools.Rectangle;
+import com.craneding.canvas.client.drawtools.CanvasTools.CanvasApp;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.event.dom.client.MouseDownEvent;
 import com.google.gwt.event.dom.client.MouseDownHandler;
 import com.google.gwt.event.dom.client.MouseMoveEvent;
@@ -13,103 +19,145 @@ import com.google.gwt.event.dom.client.MouseOutEvent;
 import com.google.gwt.event.dom.client.MouseOutHandler;
 import com.google.gwt.event.dom.client.MouseUpEvent;
 import com.google.gwt.event.dom.client.MouseUpHandler;
+import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTMLPanel;
 
 /**
  * @author 丁丁
  * 
  */
 public class CanvasDrawingPanel extends DialogBox implements MouseDownHandler,
-		MouseMoveHandler, MouseUpHandler, MouseOutHandler {
+		MouseMoveHandler, MouseUpHandler, MouseOutHandler, KeyUpHandler, KeyDownHandler {
+	final HTMLPanel htmlPanel = new HTMLPanel("<div id='canvasroot' onkeydown=\"javascript:alert('ffff')\"></div>");
+	final CanvasPanel canvasBox = new CanvasPanel();
+	final CanvasPanel canvasTmp = new CanvasPanel();
 
-	final CanvasPanel canvasPanel = new CanvasPanel();
-	final CanvasContext ctx = canvasPanel.getContext2D();
-	final BrushCanvas penCanvas = new BrushCanvas(ctx);
-	final RectangleCanvas rectangleCanvas = new RectangleCanvas(ctx);
-	
+	{
+		DOM.setElementAttribute(canvasBox.getElement(), "id", "box");
+		DOM.setStyleAttribute(canvasBox.getElement(), "left", "0");
+		DOM.setStyleAttribute(canvasBox.getElement(), "top", "20");
+		DOM.setStyleAttribute(canvasBox.getElement(), "position", "absolute");
+
+		DOM.setElementAttribute(canvasTmp.getElement(), "id", "tmp");
+		DOM.setStyleAttribute(canvasTmp.getElement(), "left", "0");
+		DOM.setStyleAttribute(canvasTmp.getElement(), "top", "20");
+		DOM.setStyleAttribute(canvasTmp.getElement(), "position", "absolute");
+	}
+
+	final CanvasContext ctxBox = canvasBox.getContext2D();
+	final CanvasContext ctxTmp = canvasTmp.getContext2D();
+
+	final CanvasApp app = new CanvasApp(ctxTmp, 0, 0);
+
+	final Pencil pencil = new Pencil(app);
+	final Ellipse ellipse = new Ellipse(app);
+	final Rectangle rectangle = new Rectangle(app);
+
+	int x1 = 0;
+	int y1 = 0;
 	boolean down = false;
-	int x_s = 0;
-	int y_s = 0;
 
 	public CanvasDrawingPanel() {
-		setWidget(canvasPanel);
+		super(false, false);
+		
+		setWidget(htmlPanel);
+
+		htmlPanel.add(canvasBox, "canvasroot");
+		htmlPanel.add(canvasTmp, "canvasroot");
+
 		setStyleName("Canvas-DrawingPanel");
 
-		canvasPanel.addMouseDownHandler(this);
-		canvasPanel.addMouseMoveHandler(this);
-		canvasPanel.addMouseUpHandler(this);
-		canvasPanel.addMouseOutHandler(this);
+		canvasTmp.addMouseDownHandler(this);
+		canvasTmp.addMouseMoveHandler(this);
+		canvasTmp.addMouseUpHandler(this);
+		canvasTmp.addMouseOutHandler(this);
+		
+		addDomHandler(this, KeyUpEvent.getType());
+		addDomHandler(this, KeyDownEvent.getType());
+		
+		addKeyBoardEvent();
 	}
 	
 	@Override
-	public void show() {
-		super.show();
-		drawHell();
-	}
-	
-	void drawHell() {
-		ctx.beginPath();
-		ctx.arc(75, 75, 50, 0, (Math.PI * 2), true); // Outer circle
-		ctx.moveTo(110, 75);
-		ctx.arc(75, 75, 35, 0, Math.PI, false); // Mouth (clockwise)
-		ctx.moveTo(65, 65);
-		ctx.arc(60, 65, 5, 0, (Math.PI * 2), true); // Left eye
-		ctx.moveTo(95, 65);
-		ctx.arc(90, 65, 5, 0, (Math.PI * 2), true); // Right eye
-		ctx.stroke();
-					
-		ctx.beginPath();
-		ctx.moveTo(40, 75);
-		ctx.lineTo(60, 65);
-		ctx.lineTo(90, 65);
-		ctx.moveTo(110, 75);
-		ctx.lineTo(125, 75);
-		ctx.stroke();
+	public void center() {
+		super.center();
+		app.width = canvasTmp.getOffsetWidth();
+		app.height = canvasTmp.getOffsetHeight();
 	}
 
 	@Override
 	public void setSize(String width, String height) {
-		canvasPanel.setSize(width, height);
+		htmlPanel.setSize(width, height);
+		canvasBox.setSize(width, height);
+		canvasTmp.setSize(width, height);
 	}
 
 	@Override
 	public void onMouseDown(MouseDownEvent event) {
-		if (event.getSource() == canvasPanel) {
+		if (event.getSource() == canvasTmp) {
 			down = true;
-			x_s = event.getRelativeX(canvasPanel.getElement());
-			y_s = event.getRelativeY(canvasPanel.getElement());
-			rectangleCanvas.init(x_s, y_s);
+			x1 = event.getRelativeX(canvasTmp.getElement());
+			y1 = event.getRelativeY(canvasTmp.getElement());
+			
+			if("pencil".equalsIgnoreCase(CanvasSystem.getProperty("canvasToolName")))
+				pencil.moveTo(x1, y1);
 		}
 	}
-	
+
 	@Override
 	public void onMouseMove(MouseMoveEvent event) {
-		if (event.getSource() == canvasPanel && down) {
-			int x_e = event.getRelativeX(canvasPanel.getElement());
-			int y_e = event.getRelativeY(canvasPanel.getElement());
+		if (event.getSource() == canvasTmp && down) {
+			int x2 = event.getRelativeX(canvasTmp.getElement());
+			int y2 = event.getRelativeY(canvasTmp.getElement());
 			
-			//penCanvas.Color("orange").LineWidth(10).draw(x_s, y_s, x_e, y_e);
-			rectangleCanvas.draw(x_e, y_e);
-			
-			x_s = x_e;
-			y_s = y_e;
+			if("pencil".equalsIgnoreCase(CanvasSystem.getProperty("canvasToolName")))
+				pencil.draw(x2, y2);
+			else if("rectangle".equalsIgnoreCase(CanvasSystem.getProperty("canvasToolName")))
+				rectangle.draw(x1, y1, x2, y2);
+			else if("ellipse".equalsIgnoreCase(CanvasSystem.getProperty("canvasToolName")))
+				ellipse.draw(x1, y1, x2, y2);
 		}
 	}
 
 	@Override
 	public void onMouseUp(MouseUpEvent event) {
-		if(event.getSource() == canvasPanel) {
+		if (event.getSource() == canvasTmp && down) {
 			down = false;
-			rectangleCanvas.clear();
+			ctxBox.drawImage(canvasTmp.getCanvas(), 0, 0);
 		}
 	}
 
 	@Override
 	public void onMouseOut(MouseOutEvent event) {
-		if(event.getSource() == canvasPanel) {
+		if (event.getSource() == canvasTmp && down) {
 			down = false;
-			rectangleCanvas.clear();
+			ctxTmp.clearRect(0, 0, app.width, app.height);
 		}
 	}
+
+	@Override
+	public void onKeyUp(KeyUpEvent event) {
+		if (app.ctrl)
+			app.ctrl = false;
+	}
+
+	@Override
+	public void onKeyDown(KeyDownEvent event) {
+		if (down && event.isControlKeyDown())
+			app.ctrl = true;
+	}
+	
+	private native void addKeyBoardEvent() /*-{
+		var _self = this;
+		_self.@com.craneding.canvas.client.CanvasDrawingPanel::sinkEvents(I)(128 | 256 | 512);
+		function keyhandler(evt)
+		{
+			evt = evt ? evt : window.event;
+			_self.@com.craneding.canvas.client.CanvasDrawingPanel::onBrowserEvent(Lcom/google/gwt/user/client/Event;)(evt);
+		}
+		$doc.onkeydown = keyhandler;
+		$doc.onkeyup = keyhandler;
+	}-*/;
 
 }
