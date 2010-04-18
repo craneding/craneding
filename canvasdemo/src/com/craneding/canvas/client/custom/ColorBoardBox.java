a9cb7cb591a72b80c06b41481e2c952779c190cb
crane.ding <crane.ding@0cf19388-cbb7-11de-b058-13285e7dd63e>
2010-04-18 12:41:37 +0000

    修改界面

--- canvasdemo/src/com/craneding/canvas/client/custom/ColorBoardBox.java
+++ canvasdemo/src/com/craneding/canvas/client/custom/ColorBoardBox.java
@@ -3,11 +3,7 @@
  */
 package com.craneding.canvas.client.custom;
 
-import com.craneding.canvas.client.ui.CanvasContext;
-import com.craneding.canvas.client.ui.CanvasPanel;
 import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.HTML;
 
 /**
  * 调色板
@@ -15,75 +11,17 @@ import com.google.gwt.user.client.ui.HTML;
  * @author 丁丁
  */
 public class ColorBoardBox extends DialogBox {
-	final FlowPanel flowPanel = new FlowPanel();
-	final CanvasPanel redCanvas = new CanvasPanel();
-	final CanvasPanel greenCanvas = new CanvasPanel();
-	final CanvasPanel blueCanvas = new CanvasPanel();
 
-	final HTML redMove = new HTML();
-	final HTML greenMove = new HTML();
-	final HTML blueMove = new HTML();
-
-	final String width = "226px";
-	final int height = 18;
-
+	private ColorBoardTemplatePanel templatePanel = new ColorBoardTemplatePanel();
+	
 	public ColorBoardBox() {
 		super(false, false);
-
-		int r = 255, g = 128, b = 128, a = 1;
-
-		flowPanel.add(redCanvas);
-		flowPanel.add(greenCanvas);
-		flowPanel.add(blueCanvas);
 		
-		/*
-		flowPanel.add(redMove);
-		flowPanel.add(greenMove);
-		flowPanel.add(blueMove);
-		*/
+		setAnimationEnabled(true);
 		
-		redCanvas.setSize(width, height + "px");
-		greenCanvas.setSize(width, height + "px");
-		blueCanvas.setSize(width, height + "px");
-
-		redMove.setStyleName("select");
-		greenMove.setStyleName("select");
-		blueMove.setStyleName("select");
-		flowPanel.setStyleName("ColorPalette");
-
-		/*
-		DOM.setStyleAttribute(greenMove.getElement(), "top", "18px");
-		DOM.setStyleAttribute(greenMove.getElement(), "left", "128px");
-		DOM.setStyleAttribute(blueMove.getElement(), "top", "36px");
-		DOM.setStyleAttribute(blueMove.getElement(), "left", "128px");
-		*/
-
-//		setWidget(flowPanel);
-		setWidget(new ColorBoardTemplatePanel());
-		setStyleName("ColorPaletteBox");
-
-		draw(redCanvas.getContext2D(), r, g, b, a);
-		draw(greenCanvas.getContext2D(), r, g, b, a);
-		draw(blueCanvas.getContext2D(), r, g, b, a);
-	}
-
-	void draw(CanvasContext ctx, int r, int g, int b, int a) {
-		ctx.clearRect(0, 0, getOffsetWidth(), height);
-
-		int max = 255;
-		for (int i = max; i >= 0; i--) {
-			if (ctx == redCanvas.getContext2D())
-				r = i;
-			else if (ctx == greenCanvas.getContext2D())
-				g = i;
-			if (ctx == blueCanvas.getContext2D())
-				b = i;
-
-			ctx.beginPath();
-			ctx.fillStyle("rgba(" + r + "," + g + "," + b + "," + a + ")");
-			ctx.fillRect(max - i, 0, 1, height);
-			ctx.closePath();
-		}
+		setWidget(templatePanel);
+		
+		setStyleName("ColorBoardBox");
 	}
 
 }
--- canvasdemo/src/com/craneding/canvas/client/custom/ColorBoardTemplatePanel.java
+++ canvasdemo/src/com/craneding/canvas/client/custom/ColorBoardTemplatePanel.java
@@ -6,6 +6,17 @@ package com.craneding.canvas.client.custom;
 import com.craneding.canvas.client.template.HTMLTemplatePanel;
 import com.craneding.canvas.client.ui.CanvasContext;
 import com.craneding.canvas.client.ui.CanvasPanel;
+import com.google.gwt.event.dom.client.MouseDownEvent;
+import com.google.gwt.event.dom.client.MouseDownHandler;
+import com.google.gwt.event.dom.client.MouseMoveEvent;
+import com.google.gwt.event.dom.client.MouseMoveHandler;
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.MouseUpEvent;
+import com.google.gwt.event.dom.client.MouseUpHandler;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Widget;
 
 /**
  * 调色板模板界面
@@ -13,7 +24,8 @@ import com.craneding.canvas.client.ui.CanvasPanel;
  * @author 丁丁
  * 
  */
-public class ColorBoardTemplatePanel extends HTMLTemplatePanel {
+public class ColorBoardTemplatePanel extends HTMLTemplatePanel implements
+		MouseDownHandler, MouseUpHandler, MouseOutHandler, MouseMoveHandler {
 	CanvasPanel redCanvas;
 	CanvasPanel greenCanvas;
 	CanvasPanel blueCanvas;
@@ -21,6 +33,27 @@ public class ColorBoardTemplatePanel extends HTMLTemplatePanel {
 	CanvasContext redctx;
 	CanvasContext greenctx;
 	CanvasContext bluectx;
+
+	HTML redNum;
+	HTML greenNum;
+	HTML blueNum;
+	
+	HTML redIcon;
+	HTML greenIcon;
+	HTML blueIcon;
+
+	RGBA rgba = new RGBA();
+	
+	int w = 226, h = 18, max = 256;
+
+	boolean down = false;
+	
+	{
+		rgba.r = 255;
+		rgba.g = 128;
+		rgba.b = 128;
+		rgba.alpha = 100;
+	}
 
 	public ColorBoardTemplatePanel() {
 		super("../template/colorBoardTemplate.html");
@@ -36,34 +69,150 @@ public class ColorBoardTemplatePanel extends HTMLTemplatePanel {
 		greenCanvas = (CanvasPanel) getAutoWidget("${green}");
 		blueCanvas = (CanvasPanel) getAutoWidget("${blue}");
 
+		redNum = (HTML) getAutoWidget("${red-num}");
+		greenNum = (HTML) getAutoWidget("${green-num}");
+		blueNum = (HTML) getAutoWidget("${blue-num}");
+		
+		redIcon = (HTML) getAutoWidget("${red-icon}");
+		greenIcon = (HTML) getAutoWidget("${green-icon}");
+		blueIcon = (HTML) getAutoWidget("${blue-icon}");
+
+		redNum.setText(rgba.r + "");
+		greenNum.setText(rgba.g + "");
+		blueNum.setText(rgba.b + "");
+
 		redctx = redCanvas.getContext2D();
 		greenctx = greenCanvas.getContext2D();
 		bluectx = blueCanvas.getContext2D();
 
-		int dr = 255, dg = 128, db = 128, da = 1, w = 226, h = 18;
-
-		draw(redctx, dr, dg, db, da, w, h);
-		draw(greenctx, dr, dg, db, da, w, h);
-		draw(bluectx, dr, dg, db, da, w, h);
+		draw(redctx, rgba.copy(), w, h);
+		draw(greenctx, rgba.copy(), w, h);
+		draw(bluectx, rgba.copy(), w, h);
+		
+		redCanvas.addMouseDownHandler(this);
+		redCanvas.addMouseMoveHandler(this);
+		redCanvas.addMouseOutHandler(this);
+		redCanvas.addMouseUpHandler(this);
+		greenCanvas.addMouseDownHandler(this);
+		greenCanvas.addMouseMoveHandler(this);
+		greenCanvas.addMouseOutHandler(this);
+		greenCanvas.addMouseUpHandler(this);
+		blueCanvas.addMouseDownHandler(this);
+		blueCanvas.addMouseMoveHandler(this);
+		blueCanvas.addMouseOutHandler(this);
+		blueCanvas.addMouseUpHandler(this);
 	}
 
-	void draw(CanvasContext ctx, int r, int g, int b, int a, int w, int h) {
+	void draw(CanvasContext ctx, RGBA rgba, int w, int h) {
 		ctx.clearRect(0, 0, w, h);
-
-		int max = 255;
+		
 		for (int i = max; i >= 0; i = i - 2) {
 			if (ctx == redctx)
-				r = i;
+				rgba.r = i;
 			else if (ctx == greenctx)
-				g = i;
+				rgba.g = i;
 			if (ctx == bluectx)
-				b = i;
+				rgba.b = i;
 
 			ctx.beginPath();
-			ctx.fillStyle("rgba(" + r + "," + g + "," + b + "," + a + ")");
+			ctx.fillStyle("rgba(" + rgba.r + "," + rgba.g + "," + rgba.b + "," + rgba.alpha + ")");
 			ctx.fillRect(max - i, 0, 2, h);
 			ctx.closePath();
 		}
 	}
 
+	public RGBA rgba() {
+		return rgba;
+	}
+
+	public static class RGBA {
+		int r;
+		int g;
+		int b;
+		int alpha;/* 0-100 */
+
+		public RGBA copy() {
+			RGBA copy = new RGBA();
+			copy.r = this.r;
+			copy.g = this.g;
+			copy.b = this.b;
+			copy.alpha = this.alpha;
+			return copy;
+		}
+	}
+
+	@Override
+	public void onMouseDown(MouseDownEvent event) {
+		down = true;
+		
+		Object source = event.getSource();
+		
+		int x1 = event.getRelativeX(((Widget)source).getElement());
+		int x2 = max - x1 * 2;
+		
+		if(source == redCanvas) {
+			selectRed(x1, x2);
+		} else if(source == greenCanvas) {
+			selectGreen(x1, x2);
+		} else if(source == blueCanvas) {
+			selectBlue(x1, x2);
+		}
+	}
+
+	@Override
+	public void onMouseUp(MouseUpEvent event) {
+		Object source = event.getSource();
+		if(source == redCanvas || source == greenCanvas || source == blueCanvas)
+			down = false;
+	}
+
+	@Override
+	public void onMouseOut(MouseOutEvent event) {
+		Object source = event.getSource();
+		if(source == redCanvas || source == greenCanvas || source == blueCanvas)
+			down = false;
+	}
+
+	@Override
+	public void onMouseMove(MouseMoveEvent event) {
+		if(down) {
+			Object source = event.getSource();
+			int x1 = event.getRelativeX(((Widget)source).getElement());
+			int x2 = max - x1 * 2;
+			
+			if(source == redCanvas) {
+				selectRed(x1, x2);
+			} else if(source == greenCanvas) {
+				selectGreen(x1, x2);
+			} else if(source == blueCanvas) {
+				selectBlue(x1, x2);
+			}
+		}
+	}
+
+	private void selectBlue(int x1, int x2) {
+		DOM.setStyleAttribute(blueIcon.getElement(), "left", x1 + 16 + "px");
+		blueNum.setText(x2 + "");
+		rgba.b = x2;
+		draw(redCanvas.getContext2D(), rgba.copy(), w, h);
+		draw(greenCanvas.getContext2D(), rgba.copy(), w, h);
+	}
+
+	private void selectGreen(int x1, int x2) {
+		DOM.setStyleAttribute(greenIcon.getElement(), "left", x1 + 16 + "px");
+		greenNum.setText(x2 + "");
+		rgba.g = x2;
+		draw(redCanvas.getContext2D(), rgba.copy(), w, h);
+		draw(blueCanvas.getContext2D(), rgba.copy(), w, h);
+	}
+
+	private void selectRed(int x1, int x2) {
+		DOM.setStyleAttribute(redIcon.getElement(), "left", x1 + 16 + "px");
+		redNum.setText(x2 + "");
+		rgba.r = x2;
+		draw(greenCanvas.getContext2D(), rgba.copy(), w, h);
+		draw(blueCanvas.getContext2D(), rgba.copy(), w, h);
+	}
+	
+	
 }
